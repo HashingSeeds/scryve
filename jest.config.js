2585ca4eb7093a22e997baf4dc6b05f08ff0d391
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2026-08-25 05:29:30 +0000

    Cache Jest results in CI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@jest/types').Config.ProjectConfig} */
 module.exports = {
   preset: "jest-expo",
+  cacheDirectory: "<rootDir>/.jest-cache",
   setupFiles: ["<rootDir>/test/setup.ts"],
   transformIgnorePatterns: [
     "/node_modules/(?!(.pnpm|react-native|@react-native|@react-native-community|expo|@expo|@expo-google-fonts|react-navigation|@react-navigation|@sentry/react-native|native-base|convex-test|react-native-purchases|react-native-purchases-ui|@revenuecat))",
